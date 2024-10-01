324defdf1435f49ea6b36b74120b0b3b00f8a10e
Hanif <hanif@Hanifs-MacBook-Air.local>
2024-10-01 19:47:19 +0600

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { MdOutlineAddBox } from "react-icons/md";
 import { Link } from "react-router-dom";
-import BooksCard from "../../components/home/BooksCard.jsx";
 import BooksTable from "../../components/home/BooksTable.jsx";
 import Spinner from "./../../components/Spinner.jsx";
 
 export default function Home() {
   const [books, setBooks] = useState([]);
-  const [showType, setShowType] = useState("table");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -29,35 +27,13 @@ export default function Home() {
 
   return (
     <div className="p-4">
-      <div className="flex justify-center items-center gap-x-4">
-        <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-          onClick={() => setShowType("table")}
-        >
-          Table
-        </button>
-        <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-          onClick={() => setShowType("card")}
-        >
-          Card
-        </button>
-      </div>
-
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
-
-      {loading ? (
-        <Spinner />
-      ) : showType === "table" ? (
-        <BooksTable books={books} />
-      ) : (
-        <BooksCard books={books} />
-      )}
+      {loading ? <Spinner /> : <BooksTable books={books} />}
     </div>
   );
 }
